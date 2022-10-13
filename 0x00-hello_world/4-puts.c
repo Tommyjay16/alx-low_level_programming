1a04ad0827bb4175972d8a1dfce7d4210c9d6112
Tommy Jay <alausatomiwa2000@gmail.com>
2022-10-13 15:30:09 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
+
+
+
 /**
- * main - Entry
- * Return: Always 0
- */
+ *
+ *  * main - prints the phrase "Programming is like building a multilingual puzzle
+ *
+ *   * Return: 0 
+ *
+ *    */
+
+
+
 int main(void)
 
-	puts("\Programming is like building a multilingual puzzle");
-	return (0);
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
