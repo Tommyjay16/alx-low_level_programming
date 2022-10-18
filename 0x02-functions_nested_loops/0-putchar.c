49f89bf5a43998ffddc421cbf1e4fa30cd7a570b
Tommyjay16 <105993515+Tommyjay16@users.noreply.github.com>
2022-10-18 12:33:36 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,22 @@
 #include "main.h"
 
-/*
- * Main Entry Point
- * Return: Return Void
+/**
+ * main - Entry point
+ *
+ * Return: nothing, return void
  */
-/*This is a void return*/
+
 int main(void)
 {
-	_putchar ("_putchar");
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
 }
